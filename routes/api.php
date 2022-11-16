c363177480770f3fc7c9e77f3148ebc2b0843190
Anne Wright <anne.wright@iadt.ie>
2022-11-16 11:10:22 +0000

    Added Authentication routes. Also grouped some routes so they can only be accessed when logged in - ie. the request comes with a token.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\PublisherController;
 use Illuminate\Http\Request;
@@ -16,16 +17,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+// every route within these curly brackets require authentication token
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/auth/logout',[AuthController::class, 'logout']);
+    Route::get('/auth/user',[AuthController::class, 'user']);
+
+    // You need to be logged in for all book functionality except get all and get by id
+    Route::apiResource('/books', BookController::class)->except((['index', 'show']));
 });
 
 // This one line creates all routes for the BookController
 // if you are not providing all actions the you don't need all the routes
 // so you can specfiy the routes individually
-Route::apiResource('/books', BookController::class);
+//Route::apiResource('/books', BookController::class);
 
-// this line create all routes for PublisherController
+// Books - Define the get all and get by ID routes outside the authentication group
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+
+
+// this line create all routes for PublisherController.
+// These routes can all be accessed without an authentication token.
+// How would you alter this so you need authentication for publisher routes that alter the database?
 Route::apiResource('/publishers', PublisherController::class);
 
+// This will define the route for Author, the implementation is not yet completed, so it's commented out.
 // Route::resource('/authors', AuthorController::class)->only(['index', 'show']);
